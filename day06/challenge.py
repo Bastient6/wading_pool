ca5768fb92f14c83c495640505e2d6bc17a8524e
bastient6 <bastien.tiffy@epitech.eu>
2025-09-15 10:11:31 +0200

    [ADD]_challenge

--- day06/challenge.py
+++ day06/challenge.py
@@ -1,7 +1,13 @@
+import time
 def power(number, powe):
     res = number
     for i in range (1, powe):
         res *= number
     print(res)
 
-power(42, 168)
+def func():
+    start = time.time()
+    power(42, 168)
+    print(time.time() - start)
+
+func()
